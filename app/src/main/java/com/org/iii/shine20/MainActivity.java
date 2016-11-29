1106451637c3fa4c6cb0147d646326039db23320
user <a>
2016-11-29 10:17:33 +0800

    v4 行動裝置

--- app/src/main/java/com/org/iii/shine20/MainActivity.java
+++ app/src/main/java/com/org/iii/shine20/MainActivity.java
@@ -3,8 +3,13 @@ package com.org.iii.shine20;
 import android.Manifest;
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.content.ContentResolver;
 import android.content.pm.PackageManager;
-import android.net.Uri;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -13,28 +18,35 @@ import android.os.Bundle;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.widget.ImageView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
+import static android.content.Context.TELEPHONY_SERVICE;
 
 public class MainActivity extends AppCompatActivity {
     private TelephonyManager tmgr;
     private AccountManager amgr;
+    private ContentResolver contentResolver;
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        img = (ImageView)findViewById(R.id.img);
+
+        contentResolver = getContentResolver();
+
         // 取得權限
         if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.GET_ACCOUNTS)
+                Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.READ_PHONE_STATE,
-                            Manifest.permission.GET_ACCOUNTS},
+                            Manifest.permission.GET_ACCOUNTS,
+                            Manifest.permission.READ_CONTACTS,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                    },
                     123);
         }else{
             init();
@@ -43,18 +55,60 @@ public class MainActivity extends AppCompatActivity {
 
     private void init(){
         tmgr = (TelephonyManager)getSystemService(TELEPHONY_SERVICE);
-        Log.v("shine", "機馬" +tmgr.getDeviceId());
-        Log.v("shine", "sim" +tmgr.getSubscriberId());
-
+        Log.v("brad", "機馬" +tmgr.getDeviceId());
+        Log.v("brad", "ｓｉｍ" +tmgr.getSubscriberId());
         tmgr.listen(new MyPhoneStateListener(), PhoneStateListener.LISTEN_CALL_STATE);
-
         // 取得帳號
         amgr = (AccountManager)getSystemService(ACCOUNT_SERVICE);
         Account[] as = amgr.getAccounts();
         for (Account a: as){
-            Log.v("shine", a.name + ":" +a.type );
+            Log.v("brad", a.name + ":" +a.type );
         }
+
+        //getContact();
+        getPhoto();
+
+
     }
+
+    //聯絡人
+    private void getContact(){
+        Log.v("brad", "OK");
+
+        String[] projection = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER
+        };
+        Cursor cursor = contentResolver.query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                projection, null, null,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+
+        Log.v("brad", "count: " + cursor.getCount());
+
+        while ( cursor.moveToNext()){
+            String name = cursor.getString(0);
+            String tel = cursor.getString(1);
+            Log.v("brad", name + ":" + tel);
+        }
+
+    }
+
+    //取得照片
+
+    private void getPhoto(){
+        Cursor c = contentResolver.query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null, null, null, null);
+        //Log.v("brad", "photo: " + c.getCount());
+
+        c.moveToLast();
+        String data = c.getString(c.getColumnIndex(MediaStore.Images.Media.DATA));
+        Log.v("brad", "photo: " + data);
+
+        Bitmap photo = BitmapFactory.decodeFile(data);
+        img.setImageBitmap(photo);
+
+    }
+
 
     private class MyPhoneStateListener extends PhoneStateListener {
         @Override
